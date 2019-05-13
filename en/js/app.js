f63a6647c04001141a6a2a0205089461865ecbd5
sergey <sergey@guests-MBP.Dlink>
2019-05-13 14:43:30 +0300

    bxslider fix link

--- en/js/app.js
+++ en/js/app.js
@@ -205,7 +205,8 @@ var APP = {
             pager: false,
             prevText: '',
             nextText: '',
-            adaptiveHeight: adaptiveHeight
+            adaptiveHeight: adaptiveHeight,
+            touchEnabled: false
         });
     },
 
